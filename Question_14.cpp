79de7fc968432b6945588a609c5feec8e09be4ee
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-10-06 23:27:31 +0530

    Create Question_14.cpp

---
+++ Question_14.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int array(int x[],int n)
+{
+  int sum;
+  if(n==0)
+     sum=0;
+  else
+     sum=x[n-1]+array(x,n-1);
+  return (sum);
+}
+
+int main()
+{
+   int n[100],l;
+   cout << "Enter the number of element of array(0 to 100) : ";
+   cin >> l;
+   cout << "Enter element of array(0 tom 100) : ";
+   for(int k=0;k<l;k++)
+     cin >> n[k];
+   cout << endl<<"Sum of the  element of the array : "<<array(n,l);
+   return 0;
+}
